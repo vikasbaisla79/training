f4e5b5d5c7b7e6a07fa0454a990f1cb7d388bc9f
Vikas Baisla <vikasbaisla79@gmail.com>
2022-06-02 21:37:42 +0530

    [Add]Added program for Check Leap year

---
+++ src/1_June/Q20.java
@@ -0,0 +1,19 @@
+import java.util.Calendar;
+import java.util.Scanner;
+public class Q20{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter year");
+        int year=sc.nextInt();
+        if(year%4==0 && ( year%100!=0 || year%400==0))
+        {
+            System.out.println(year+" is a leap year");
+        }
+        else
+        {
+            System.out.println(year+" is not a leap year");
+        }
+
+    }
+
+}
