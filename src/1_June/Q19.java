0bad5253ea11b0605bec79a91d1e91669bf9091d
Vikas Baisla <vikasbaisla79@gmail.com>
2022-06-02 21:36:47 +0530

    [Add]Added program for Check perfect number

---
+++ src/1_June/Q19.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Q19 {
+
+    public static void main(String[] args) {
+
+        int n, sum = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        n = s.nextInt();
+
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                sum = sum + i;
+
+            }
+
+        }
+        if (sum == n) {
+            System.out.println("Given number is Perfect Number");
+
+        } else {
+            System.out.println("Given number is not Perfect Number");
+
+        }
+
+    }
+
+}
