15a48c39f3de63dc03aa75092cfec4757257db73
Vikas Baisla <vikasbaisla79@gmail.com>
2022-06-09 08:48:30 +0530

    [Add] Digit Replacer Program

---
+++ src/8_June/Q42.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Q42 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any String with numeric character : ");
+        String str = sc.next();
+        String result = new String("");
+        for(int i=0;i<str.length();i++){
+            if(Character.getNumericValue(str.charAt(i)) < 5) result += 0;
+            else result += 1;
+        }
+        System.out.println(result);
+    }
+}
